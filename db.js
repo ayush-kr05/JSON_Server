735986154db00da68841528346d48bb518018d1f
ayush-kr05 <ayushkumar.barbighaiya@gmail.com>
2022-08-10 23:57:33 +0530

    First Server Commit

---
+++ db.js
@@ -0,0 +1,11 @@
+const items = require("./data/items");
+const tags = require("./data/tags");
+const brands = require("./data/brands");
+const itemTypes = require("./data/itemTypes");
+
+module.exports = {
+  items,
+  tags,
+  brands,
+  itemTypes,
+};
---
+++ server.js
@@ -0,0 +1,12 @@
+const data = require("./db.js");
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
