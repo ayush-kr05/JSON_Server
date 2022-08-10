157c0c79ed5850356cdc4816af4b3110d513a592
ayush-kr05 <ayushkumar.barbighaiya@gmail.com>
2022-08-11 00:22:04 +0530

    jio home data added

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
+const jiomarthomedata = require("./data/jiomarthomedata");
 
 module.exports = {
   items,
   tags,
   brands,
   itemTypes,
+  jiomarthomedata,
 };
