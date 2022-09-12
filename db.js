4f0ab8d14c44670fc60dd6e80b0953faf929f9df
ayush-kr05 <ayushkumar.barbighaiya@gmail.com>
2022-09-12 22:37:36 +0530

    Dermstore Heroku

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
 const jiomarthomedata = require("./data/jiomarthomedata.json");
 const react = require("./React/react.json");
-const dermcart = require("./dermcart.json");
-const dermproducts = require("./dermproducts.json");
-const dermuserinfo = require("./dermuserinfo.json");
+const dermcart = require("./DermStore/dermcart.json");
+const dermproducts = require("./DermStore/dermproducts.json");
+const dermuserinfo = require("./DermStore/dermuserinfo.json");
 
 module.exports = {
   items,
